1d9bf1495bc1935846c6e04f51a3d1ab10fa8b68
dlapochkin <d.lapochkin@g.nsu.ru>
2021-02-15 08:42:52 +0500

    first

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+from textblob import TextBlob
+text = input()
+blob = TextBlob(text)
+if blob.detect_language() == 'ru':
+    blob.translate(to="en")
+if blob.sentiment.polarity > 0.33:
+    tonality = 'Положительная'
+elif blob.sentiment.polarity < -0.33:
+    tonality = 'Отрицательная'
+else:
+    tonality = 'Нейтральная'
+print('Тональность текста:', tonality)
+obj = format(1 - blob.sentiment.subjectivity, '.1%')
+print('Объективность:', obj)
