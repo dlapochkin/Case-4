3439d7834ac79cea10145bfa61d9e8fd6494bfd2
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-02-15 15:19:18 +0700

    start

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from textblob import TextBlob
 text = input()
 syllables = 0
 sentence = 0
+fre = 0
 
 for b in ['.', '!', '?']:
     if text.count(b) > 0:
@@ -18,7 +19,11 @@ else:
 print('Слогов:', syllables)
 print('Средняя длина предложения в словах:', (text.count(' ') + 1) / sentence)
 print('Средняя длина слова в слогах:', syllables / (text.count(' ') + 1))
-
+if ord(text[0]) < 122:
+    fre = 206.835 - (1.3 * asl) - (60.1 * asw)
+else:
+    fre = 206.835 - (1.015 * asl) - (84.6 * asw)
+print('Индекс удобочитаемости Флеша:', fre)
 blob = TextBlob(text)
 if blob.detect_language() == 'ru':
     blob = blob.translate(to="en")
