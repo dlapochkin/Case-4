e4c17c350e2daea0dfa37b2fa8ab4500dcef702d
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-02-15 14:39:16 +0700

    start

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from textblob import TextBlob
-text = input()
-syllables=0
-sentence=0
+import textblob
 
-for b in['.','!','?']:
-    if text.count(b)>0:
+text = input()
+syllables = 0
+sentence = 0
+
+for b in ['.', '!', '?']:
+    if text.count(b) > 0:
         sentence += text.count(b)
 print('Предложений:', sentence)
 
-print('Слов:',text.count(' ')+1 )
+print('Слов:', text.count(' ') + 1)
 
-if ord(text[0])<122:
-    syllables=sum(1 for x in text.lower() if x in 'aeiou')
+if ord(text[0]) < 122:
+    syllables = sum(1 for x in text.lower() if x in 'aeiou')
 else:
     syllables = sum(1 for x in text.lower() if x in 'уеоаыяиюэ')
-print('Слогов:',syllables)
-print('Средняя длина предложения в словах:',text.count(' ')+1/sentence)
-print('Средняя длина слова в слогах:',syllables/text.count(' ')+1)
+print('Слогов:', syllables)
+print('Средняя длина предложения в словах:', (text.count(' ') + 1) / sentence)
+print('Средняя длина слова в слогах:', syllables / (text.count(' ') + 1))
 
 blob = TextBlob(text)
 if blob.detect_language() == 'ru':
